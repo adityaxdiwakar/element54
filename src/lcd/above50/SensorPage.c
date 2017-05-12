dd8b49edcdec9c95cdc3a6e3f7ce0b58bdd7a4f8
Aditya Diwakar <aditya.diwakar2009@gmail.com>
2017-05-12 14:14:05 -0400

    Added sensor page w/ changes

--- src/lcd/above50/SensorPage.c
+++ src/lcd/above50/SensorPage.c
@@ -1,19 +1,6 @@
 #include "lcdRuling.h"
 
 void Pg_Sensor() {
-  lcdSetText(uart1, 1, "Sensors?");
+  lcdPrint(uart1, 1, "G: %d AP: %d", gyro, autopot);
   lcdSetText(uart1, 2, "<      >");
-
-  if (lcdReadButtons(uart1) == 2) {
-    int PgNum = PgNum * 10;
-    pageChecker();
-  }
-  if (PgNum == PgNum * 10) { //Gyro page
-    lcdSetText(uart1, 1, "Gyro is");
-    lcdPrint(uart1, 2, "%d", gyro);
-  }
-  if (PgNum == PgNum * 10 + 1) { //Autonomous potentiometer page
-    lcdSetText(uart1, 1, "Auton Selector");
-    lcdPrint(uart1, 2, "%d", autopot);
-  }
 }
